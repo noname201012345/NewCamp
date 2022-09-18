06223a07239b8704a6cdb786a201520d55e9ce4a
noname201012345 <58730410+noname201012345@users.noreply.github.com>
2022-09-18 12:52:40 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+import os
+from discord.ext import commands
+
+client=commands.Bot(command_prefix=':', self_bot=True, help_command=None)
+
+GUILD_ID = 755793441287438469
+CHANNEL_ID = 994552773637062656
+
+@client.event
+async def on_ready():
+    os.system('clear')
+    print(f'Logged in as {client.user} ({client.user.id})')
+    vc = discord.utils.get(client.get_guild(GUILD_ID).channels, id = CHANNEL_ID)
+    await vc.connect()
+    print(f"Successfully joined {vc.name} ({vc.id})")
+
+client.run(os.getenv("TOKEN"))
