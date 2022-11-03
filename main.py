c9957f9a7b54dbadf94882df3b5083232871f30e
noname201012345 <58730410+noname201012345@users.noreply.github.com>
2022-11-03 16:26:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ CHANNEL_ID = 994552773637062656
 
 rtoken = os.getenv("RTOKEN")
 header = {"Authorization": "Bearer {}".format(rtoken)}
-link="https://api.github.com/repos/noname201012345/MainAcc/contents/"
+link="https://api.github.com/repos/noname201012345/NewCamp/contents/"
 
 @client.event
 async def on_ready():
@@ -37,7 +37,7 @@ async def on_ready():
 async def on_call():
     while True:
         vc = discord.utils.get(client.get_guild(GUILD_ID).channels, id = CHANNEL_ID)
-        if client.get_guild(GUILD_ID).get_member(client.user.id).voice.channel is None:
+        if client.get_guild(GUILD_ID).get_member(client.user.id).voice is None:
             await vc.connect()
             print(f"Successfully joined {vc.name} ({vc.id})")
 
